ddbe2d081aa6e6a9eaab598d44daff3f48eeaff1
keonwooo <rjsdn7732@naver.com>
2024-08-26 22:59:14 +0900

    [2024.08.26] 1. TodoList CheckBox 기능 구현 2. TodoList Delete 기능 구현

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,18 +1,29 @@
 import React, { useState } from "react";
 import AddTodo from "../AddTodo/AddTodo";
+import Todo from "../Todo/Todo";
 
 export default function TodoList() {
     const [todos, setTodos] = useState([
-        {id : '123', text: '장보기', status: 'active'}
-        ,{id : '124', text: '공부하기', status: 'active'}
+        { id: '123', text: '장보기', status: 'active' }
+        , { id: '124', text: '공부하기', status: 'active' }
     ]);
-    const handleAdd = (todo) => setTodos([...todos, todo]);
+    const handleAdd = (todo) => {
+        setTodos([...todos, todo]);
+        console.log(todo);
+    }
+    const handleUpdate = (updated) => setTodos(todos.map(t => t.id === updated.id ? updated : t));
+    const handleDelete = (deleted) => setTodos(todos.filter((t) => t.id !== deleted.id));
     return <section>
         <ul>
             {todos.map((item) => (
-                <li id={item.id}>{item.text}</li>
+                <Todo
+                    key={item.id}
+                    todo={item}
+                    onUpdate={handleUpdate}
+                    onDelete={handleDelete}
+                />
             ))}
         </ul>
-        <AddTodo onAdd={handleAdd}/>
+        <AddTodo onAdd={handleAdd} />
     </section>
 }
