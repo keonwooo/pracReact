50e9f7bcad3b54f2fbcb5bdce44eca480c711017
keonwooo <rjsdn7732@naver.com>
2024-08-26 22:28:23 +0900

    [2024.08.26] 1. TodoList 항목 추가 기능

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
+import AddTodo from "../AddTodo/AddTodo";
 
 export default function TodoList() {
     const [todos, setTodos] = useState([
         {id : '123', text: '장보기', status: 'active'}
         ,{id : '124', text: '공부하기', status: 'active'}
-    ])
+    ]);
+    const handleAdd = (todo) => setTodos([...todos, todo]);
     return <section>
         <ul>
             {todos.map((item) => (
                 <li id={item.id}>{item.text}</li>
             ))}
         </ul>
+        <AddTodo onAdd={handleAdd}/>
     </section>
-    
 }
