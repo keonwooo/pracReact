fb7e54691cd97ddaec7b93074fd718f7a50df62f
keonwooo <rjsdn7732@naver.com>
2024-08-28 17:13:38 +0900

    [2024.08.28] 1. TodoList css 꾸미기_1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import styles from './Header.module.css';
 
 export default function Header(item) {
-    return <header>
-        <ul>
-            {item.filters.map((value, index) => (
-                <li key={index}>
-                    <button onClick={() => item.onFilterChange(value)}>{value}</button>
-
-                </li>
-            ))}
-        </ul>
-    </header>
+    return (
+        <header className={styles.header}>
+            <ul className={styles.filters}>
+                {item.filters.map((value, index) => (
+                    <li key={index} >
+                        <button 
+                            onClick={() => item.onFilterChange(value)} 
+                            className={`${styles.filter} ${item.filter === value && styles.selected}`}
+                        >
+                            {value}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </header>
+    )
 }
