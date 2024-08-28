af4c13e4acf531dc1c598aeb3c47ab75894cb0da
keonwooo <rjsdn7732@naver.com>
2024-08-28 17:32:30 +0900

    [2024.08.28] 1. TodoList css 수정_2

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import AddTodo from "../AddTodo/AddTodo";
 import Todo from "../Todo/Todo";
+import styles from "./TodoList.module.css";
 
 export default function TodoList({ filter }) {
     const [todos, setTodos] = useState([
@@ -15,19 +16,21 @@ export default function TodoList({ filter }) {
     const handleDelete = (deleted) => setTodos(todos.filter((t) => t.id !== deleted.id));
 
     const filtered = getFilteredItems(todos, filter);
-    return <section>
-        <ul>
-            {filtered.map((item) => (
-                <Todo
-                    key={item.id}
-                    todo={item}
-                    onUpdate={handleUpdate}
-                    onDelete={handleDelete}
-                />
-            ))}
-        </ul>
-        <AddTodo onAdd={handleAdd} />
-    </section>
+    return (
+        <section className={styles.container}>
+            <ul className={styles.list}>
+                {filtered.map((item) => (
+                    <Todo
+                        key={item.id}
+                        todo={item}
+                        onUpdate={handleUpdate}
+                        onDelete={handleDelete}
+                    />
+                ))}
+            </ul>
+            <AddTodo onAdd={handleAdd} />
+        </section>
+    )
 }
 
 function getFilteredItems(todos, filter) {
